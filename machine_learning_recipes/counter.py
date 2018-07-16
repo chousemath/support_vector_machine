a246d56c4e5c22f379f2d380f832e81ad4a9e590
Joseph Sungpil Choi <chousemath@gmail.com>
2018-07-16 11:44:04 +0900

    Modify the counter script and garbageman script to be able to operate on external harddisk

--- machine_learning_recipes/counter.py
+++ machine_learning_recipes/counter.py
@@ -2,10 +2,23 @@
 # in any give vehicle model directory, determine whether or not
 # that directory has be completely cleaned of useless images,
 # and then upload that data to a remote database
+import argparse
 from os import listdir
 from colors import bcolors
 
-ROOT = '/Users/jo/Desktop/data_science/support_vector_machine/machine_learning_recipes/tf_files/flower_photos'
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument(
+    "env", help="Determines whether you want to run this script on local or external harddisk")
+ARGS = PARSER.parse_args()
+
+# ENV should either be 'local' or 'external', garbageman.py depends on this exact distinction
+ENV = ARGS.env
+
+if ENV == 'local':
+    ROOT = '/Users/jo/Desktop/data_science/support_vector_machine/machine_learning_recipes/tf_files/flower_photos'
+elif ENV == 'external':
+    ROOT = '/Volumes/TriveStorage/ml_data'
+
 for path in listdir(ROOT):
     if path == '.DS_Store':
         continue
@@ -24,7 +37,7 @@ for path in listdir(ROOT):
         print(
             f'{bcolors.WARNING}Unchecked directory{bcolors.ENDC}, total: {total}, checked: {checked}, unchecked: {unchecked}')
         print(
-            f'{bcolors.OKGREEN}python garbageman.py {year} {make} {model} {trim}{bcolors.ENDC} &')
+            f'{bcolors.OKGREEN}python garbageman.py {year} {make} {model} {trim} {ENV}{bcolors.ENDC} &')
         print('=================================================')
     elif total < 1000:
         print(f'{bcolors.OKBLUE}Less than 1,000 images ({total}): {path}{bcolors.ENDC}')
--- machine_learning_recipes/garbageman.py
+++ machine_learning_recipes/garbageman.py
@@ -16,13 +16,21 @@ def is_good(raw_output) -> List[str]:
     return 'good' in intermediate[0][1]
 
 
+EXTERNAL_HARDDRIVE = '/Volumes/TriveStorage/ml_data'
 ROOT = '/Users/jo/Desktop/data_science/support_vector_machine/machine_learning_recipes'
 YEAR = sys.argv[1]
 MAKE = sys.argv[2]
 MODEL = sys.argv[3]
 TRIM = sys.argv[4]
+ENV = sys.argv[5]
 DIRECTORY_NAME = f'{YEAR}_{MAKE}_{MODEL}_{TRIM}'
-FILE_PATH = f'/Users/jo/Desktop/data_science/support_vector_machine/machine_learning_recipes/tf_files/flower_photos/{DIRECTORY_NAME}'
+
+# THIS NEEDS TO BE SWAPPED FOR USE ON LOCAL/EXTERNAL HARDDISK
+if ENV == 'local':
+    FILE_PATH = f'/Users/jo/Desktop/data_science/support_vector_machine/machine_learning_recipes/tf_files/flower_photos/{DIRECTORY_NAME}'
+elif ENV == 'external':
+    FILE_PATH = f'{EXTERNAL_HARDDRIVE}/{DIRECTORY_NAME}'
+
 for path in listdir(FILE_PATH):
     # make sure you don't check good images again
     if 'good-' in path:
@@ -41,6 +49,7 @@ for path in listdir(FILE_PATH):
             # end up checking it again
             os.system(f'mv {FILE_PATH}/{path} {FILE_PATH}/good-{path}')
         else:
+            # THIS NEEDS TO BE SWAPPED FOR USE ON LOCAL/EXTERNAL HARDDISK
             os.system(f'rm {FILE_PATH}/{path}')
     except Exception as err:
         print(err)
