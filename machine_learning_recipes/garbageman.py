5706e2ba6358ed21007e489ecb70a7e1b308fe03
Joseph Sungpil Choi <chousemath@gmail.com>
2018-07-16 13:21:18 +0900

    Modify the garbageman script to utilize argparse

--- machine_learning_recipes/garbageman.py
+++ machine_learning_recipes/garbageman.py
@@ -1,28 +1,37 @@
 # The purpose of this script is to clean a directory
 # of useless images
 
+import argparse
 import os
 from os import listdir
 from subprocess import Popen, PIPE, STDOUT
 from typing import List
-import sys
+
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument("year", help="Designates the year of the vehicle")
+PARSER.add_argument("make", help="Designates the make of the vehicle")
+PARSER.add_argument("model", help="Designates the model of the vehicle")
+PARSER.add_argument("trim", help="Designates the trim of the vehicle")
+PARSER.add_argument(
+    "env", help="Designates whether to use the local or external harddisk")
+ARGS = PARSER.parse_args()
 
 
 def is_good(raw_output) -> List[str]:
     """Checks whether or not an image has been classified as `good`"""
-    output = list(filter(lambda x: 'LABEL:' in x,
-                         str(raw_output).split('\\n')))
-    intermediate = list(map(lambda x: x.split('"'), output))
+    label_data = list(filter(lambda x: 'LABEL:' in x,
+                             str(raw_output).split('\\n')))
+    intermediate = list(map(lambda x: x.split('"'), label_data))
     return 'good' in intermediate[0][1]
 
 
 EXTERNAL_HARDDRIVE = '/Volumes/TriveStorage/ml_data'
 ROOT = '/Users/jo/Desktop/data_science/support_vector_machine/machine_learning_recipes'
-YEAR = sys.argv[1]
-MAKE = sys.argv[2]
-MODEL = sys.argv[3]
-TRIM = sys.argv[4]
-ENV = sys.argv[5]
+YEAR = ARGS.year
+MAKE = ARGS.make
+MODEL = ARGS.model
+TRIM = ARGS.trim
+ENV = ARGS.env
 DIRECTORY_NAME = f'{YEAR}_{MAKE}_{MODEL}_{TRIM}'
 
 # THIS NEEDS TO BE SWAPPED FOR USE ON LOCAL/EXTERNAL HARDDISK
